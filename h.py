697369c6da6e5a189de62af3ff33bb1dcf78648d
11111010111 <c0deemusiic@gmail.com>
2021-05-10 01:28:45 +0200

    my caesar cypher decoder

---
+++ h.py
@@ -0,0 +1,28 @@
+string = input("Cypher: ")
+c = 0
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+            'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a',
+            'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y', 'z']
+output = []
+a = string.split(" ")
+b = list("".join(a))
+
+
+def run(c1):
+    offset = c1
+    for x in b:
+        index = alphabet.index(x)
+        output.append(alphabet[index - offset])
+    else:
+        print(str(c1) + ": " + str("".join(output)))
+        output.clear()
+        c1 += 1
+        if c1 == 26:
+            quit()
+        run(c1)
+
+
+run(c)
