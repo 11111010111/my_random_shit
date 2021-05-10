36ba43f0b9d45565746bdcb1296be80783129ec2
11111010111 <c0deemusiic@gmail.com>
2021-05-10 08:52:13 +0200

    made simpler

--- h.py
+++ h.py
@@ -9,14 +9,12 @@ output = []
 
 
 def inputs():
-    string = input("Cypher: ")
-    a = string.split(" ")
-    b = list("".join(a))
+    b = input("Cypher: ")
     run(c, b)
 
 
-def run(c1, b):
-    offset = c1
+def run(c, b):
+    offset = c
     for x in b:
         if str(x).isalpha():
             pass
@@ -27,13 +25,12 @@ def run(c1, b):
         index = alphabet.index(x)
         output.append(alphabet[index - offset])
     else:
-        print(str(c1) + ": " + str("".join(output)))
+        print(str(c) + ": " + str("".join(output)))
         output.clear()
-        c1 += 1
-        if c1 == 27:
+        c += 1
+        if c == 27:
             print("")
             inputs()
-        run(c1, b)
-
+        run(c, b)
 
 inputs()
