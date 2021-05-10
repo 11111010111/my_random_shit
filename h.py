1656e5dda72a7c7494e0e7483841469d867d19c9
11111010111 <c0deemusiic@gmail.com>
2021-05-10 14:40:08 +0200

    fixed some other stuff

--- h.py
+++ h.py
@@ -1,4 +1,3 @@
-offset = 1
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
             'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
             's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a',
@@ -15,27 +14,24 @@ def inputs():
 
 
 def run(off, b):
-    for x in b:
-        if str(x).isalpha():
-            index = alphabet.index(x)
-            output.append(alphabet[index - off])
-        elif not str(x).isalpha():
-            if str(x) == " ":
+    for y in range(27):
+        for x in b:
+            if str(x).isalpha():
+                index = alphabet.index(x)
+                output.append(alphabet[index - off])
+            elif str(x) == " ":
                 output.append(" ")
             else:
-                output.append(str(x))
+                print("Please only use letters")
+                output.clear()
+                inputs()
         else:
-            print("Please only use letters")
+            print(str(off) + ": " + str("".join(output)))
             output.clear()
-            inputs()
-    else:
-        print(str(off) + ": " + str("".join(output)))
-        output.clear()
-        off += 1
-        if off == 27:
-            print("")
-            inputs()
-        run(off, b)
+            off += 1
+            if off == 27:
+                print("")
+                inputs()
 
 
 inputs()
