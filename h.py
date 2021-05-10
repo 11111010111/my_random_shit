e050863faee1270c686e9e1236d5e50baebf842d
11111010111 <c0deemusiic@gmail.com>
2021-05-10 13:05:16 +0200

    implemented more characters

--- h.py
+++ h.py
@@ -1,36 +1,41 @@
+offset = 1
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
             'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
             's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a',
             'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
             't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 output = []
 
 
 def inputs():
-    inpu = input("Cypher: ")
-    run(1, inpu)
+    d = input("Cypher: ")
+    run(1, d)
 
 
 def run(off, b):
-    for y in range(27):
-        for x in b:
-            if str(x).isalpha():
-                index = alphabet.index(x)
-                output.append(alphabet[index - off])
-            elif str(x) == " ":
+    for x in b:
+        if str(x).isalpha():
+            index = alphabet.index(x)
+            output.append(alphabet[index - off])
+        elif not str(x).isalpha():
+            if str(x) == " ":
                 output.append(" ")
             else:
-                print("Please only use letters")
-                output.clear()
-                inputs()
+                output.append(str(x))
         else:
-            print(str(off) + ": " + str("".join(output)))
+            print("Please only use letters")
             output.clear()
-            off += 1
-            if off == 27:
-                print("")
-                inputs()
+            inputs()
+    else:
+        print(str(off) + ": " + str("".join(output)))
+        output.clear()
+        off += 1
+        if off == 27:
+            print("")
+            inputs()
+        run(off, b)
 
 
 inputs()
